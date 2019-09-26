875309f2335d22bfc912ab237e74646f6c62e365
Shereeba Pruitt <pruitt.shereeba@gmail.com>
2019-09-26 12:59:32 -0500

    initial commit/ last view all categories p1

--- src/main/java/org/launchcode/controllers/CategoryController.java
+++ src/main/java/org/launchcode/controllers/CategoryController.java
@@ -1,15 +1,47 @@
 package org.launchcode.controllers;
 
+import org.launchcode.models.Category;
 import org.launchcode.models.data.CategoryDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("category")
-
 public class CategoryController {
     @Autowired
-    private CategoryDao categoryDao;
+    private CategoryDao categoryDao;}
+
+//index handler
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public String index(Model model) {
+        model.addAttribute("title", "Categories");
+        model.addAttribute("items", this.categoryDao.findAll());
+        return "category/index";
 }
 
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String displayAddCategories(Model model) {
+    model.addAttribute("title", "Add Category");
+    model.addAttribute(new Category());
+    return "category/add";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String add(Model model, @ModelAttribute @Valid Category category, Errors errors) {
+    if(errors.hasErrors()) {
+        model.addAttribute("title", "Add Category");
+        model.addAttribute(new Error());
+        model.addAttribute("cheeseTypes", categoryDao.findAll());
+        return "category/add";
+    }
+    categoryDao.save(category);
+    return "redirect";
+}
+}
--- src/main/java/org/launchcode/models/Category.java
+++ src/main/java/org/launchcode/models/Category.java
@@ -1,5 +1,6 @@
 package org.launchcode.models;
 
+import javax.lang.model.element.Name;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -18,13 +19,15 @@ private String name;
 
 public class Category {
     //add public getter for id.
-    public void getId() {
-
+    public int getId() {
+        return id;
     }
     public void getName() {
+        return name;
 
     }
-    public void setName() {
+    public void setName(Name name) {
+        this.name = name;
 
     }
 
